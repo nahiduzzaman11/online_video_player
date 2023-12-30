32272182bc48f30af68466dbd3c413c37d0b94de
MD.Nahiduzzaman <nahiduzzaman15-12736@diu.edu.bd>
2023-12-31 01:26:46 +0600

    home_screen_design

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,6 @@
+class AppIcons{
+  static const String basePath = "assets/icons";
+  static const String commentIcon = "$basePath/comment.svg";
+  static const String reportIcon = "$basePath/report.png";
+  static const String shareIcon = "$basePath/share.png";
+}
